4ecd012f75681ad75091c30b3d555bde2ba00464
LeondavidZipp <leondavidzipp@gmx.de>
2024-06-26 14:18:00 +0200

    fixed j error

--- src/ft_libft/ft_contains_only.c
+++ src/ft_libft/ft_contains_only.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 13:42:54 by lzipp             #+#    #+#             */
-/*   Updated: 2024/06/26 14:07:40 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/06/26 14:17:23 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ int	ft_contains_only(const char *str, const char *set)
 	i = -1;
 	while (str[++i])
 	{
-		while (set[j])
+		j = -1;
+		while (set[++j])
 		{
 			if (str[i] == set[j])
 				;
 			else if (str[i] != set[j])
 				return (0);
-			j++;
 		}
 	}
 	return (1);
