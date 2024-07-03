60aa99aa81f2976bee1313ad7ef5d30f0d6ae643
jojomo96 <jmoritz@student.42heilbronn.de>
2024-07-03 16:58:35 +0200

    add rotating

--- src/ft_vector/2d/ft_vector_2d.c
+++ src/ft_vector/2d/ft_vector_2d.c
@@ -11,18 +11,33 @@
 /* ************************************************************************** */
 
 #include "ft_vector_2d.h"
+#include <math.h>
 #include <stdio.h>
 
-t_vector_2d    ft_vector_init(double x, double y)
+t_vector_2d	ft_vector_init(double x, double y)
 {
-    t_vector_2d    vector;
+	t_vector_2d	vector;
 
-    vector.x = x;
-    vector.y = y;
-    return (vector);
+	vector.x = x;
+	vector.y = y;
+	return (vector);
 }
 
-void    ft_print_vector(t_vector_2d vector)
+void	ft_print_vector(t_vector_2d vector)
 {
-    printf("Vector: (%f, %f)\n", vector.x, vector.y);
+	printf("Vector: (%f, %f)\n", vector.x, vector.y);
+}
+
+t_vector_2d	ft_vector_perpendicular(t_vector_2d vector)
+{
+	return (ft_vector_init(-vector.y, vector.x));
+}
+
+t_vector_2d	ft_vector_rotate(t_vector_2d vector, double angle)
+{
+	t_vector_2d new_vector;
+
+	new_vector.x = vector.x * cos(angle) - vector.y * sin(angle);
+	new_vector.y = vector.x * sin(angle) + vector.y * cos(angle);
+	return (new_vector);
 }
